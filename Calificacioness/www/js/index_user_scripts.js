7d7455c8c8e06310f88ec8f844d3620302808be1
vegueta <omar-11od@hotmail.com>
2015-11-22 15:04:05 -0600

    calificaciones

---
+++ Calificacioness/www/js/index_user_scripts.js
@@ -0,0 +1,29 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  .uib_w_4 */
+    
+    
+        /* button  .uib_w_4 */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
